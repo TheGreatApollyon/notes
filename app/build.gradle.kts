73530af5e02719b26b04e185dc30be7e1528623e
Rajat <rajatpatil468@gmail.com>
2025-12-03 22:56:56 +0530

    Update gradle for version change of 1.1.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.openapps.jotter"
         minSdk = 29
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
