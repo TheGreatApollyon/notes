93641267b84a43a85f90b456c56674bf0ae87928
Rajat <rajatpatil468@gmail.com>
2025-12-05 15:44:42 +0530

    V1.2.0
    Update gradle for v1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.openappslabs.jotter"
         minSdk = 29
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
