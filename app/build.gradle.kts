75a31daf8704d214f76d1065411431e02278c11b
Rajat <rajatpatil468@gmail.com>
2025-12-06 17:26:24 +0530

    Version 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "com.openappslabs.jotter"
         minSdk = 29
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
